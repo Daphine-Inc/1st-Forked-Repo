9d4e23995e4c2258566f8c7c6edd3075d0ec0359
c-1-2 <daphinekhasiro@gmail.com>
2024-03-18 07:29:36 +0300

    first commit

---
+++ user-input.py
@@ -0,0 +1,8 @@
+name= input("What is your name?: ")
+age= int (input("How old are you?: "))
+location= input("Where do you come from?: ")
+
+print("Hello " +name)
+print("you are " + str (age) + " years old ")
+print ("and live in " + location)
+
